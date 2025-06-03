938f8285f874e3eceacd9f08270e4f0975d8ef0d
Seamoon Pandey <hackedasocial@gmail.com>
2025-06-03 18:50:22 +0545

    feat: Add Hero, Navbar, ParticleBackground, Projects, and Skills components
    - Implemented Hero component with typewriter effect and floating code snippets.
    - Created Navbar component with responsive design and scroll tracking.
    - Added ParticleBackground component for animated background effects.
    - Developed Projects component to showcase various projects with detailed descriptions.
    - Introduced Skills component to display skill levels and additional technologies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import CustomCursor from "@/components/CustomCursor";
+import Experience from "@/components/Experience";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import ParticleBackground from "@/components/ParticleBackground";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 import { useScroll } from "framer-motion";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import CustomCursor from "./components/CustomCursor";
-import ParticleBackground from "./components/ParticleBackground";
 
 export default function Home() {
   const { scrollYProgress } = useScroll();
