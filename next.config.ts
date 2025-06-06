8adf1d8803b66b88d0fd1f4dc68cc6dbc4b856ab
Seamoon Pandey <hackedasocial@gmail.com>
2025-06-06 05:50:48 +0545

    setup config for 404 prevention and static export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    /* other config options here */
+    trailingSlash: true,
 };
 
 export default nextConfig;
